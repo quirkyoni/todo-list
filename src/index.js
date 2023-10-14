f0f1984bec97307534fcda12323c0e301c0dc595
Moiz Ahmed <moizah96@gmail.com>
2023-10-13 22:49:47 -0400

    Added the title of the application

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import { Project, Task } from './classes'
 
+const main = document.querySelector("main")
 
 const testProject = new Project("hello world")
 console.log(testProject)
 
 const testTask = new Task("Task", "Hello World", "never", "high")
 console.log(testTask)
+
+// To-Do List Title
+const title = document.createElement("h1")
+title.textContent = "To-Do List"
+main.appendChild(title)
+
