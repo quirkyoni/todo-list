009bb64cf9f4109b550c8274300cc945f27ad464
Moiz Ahmed <moizah96@gmail.com>
2023-10-14 22:58:52 -0400

    Added 2 HTML forms to create a Task and a Project

--- src/index.js
+++ src/index.js
@@ -7,9 +7,3 @@ console.log(testProject)
 
 const testTask = new Task("Task", "Hello World", "never", "high")
 console.log(testTask)
-
-// To-Do List Title
-const title = document.createElement("h1")
-title.textContent = "To-Do List"
-main.appendChild(title)
-
